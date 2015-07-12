14362b722e0c16c721efb30a7159f876f1b14dd4
Andrey <vkfont@gmail.com>
2015-07-12 21:39:00 +0300

    .

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ var createHttp = require('./server');
 
 var cfg = core.common.serviceConfig(require('./config.json'), require('./package.json'));
 
-module.exports = Promise.fromNode(function (done) {  // create paukan service instance
+module.exports = Promise.fromNode(function (done) {
+    // create paukan service instance
     new core.Service(cfg, done);
-}).then(function (service) {        // create webservice instance
+}).then(function (service) {
+    // create webservice instance
     createHttp(cfg, service);
-}).then(function (koa) {
-    console.log('finished');
 });
--- server/index.js
+++ server/index.js
@@ -7,12 +7,13 @@ module.exports = function (cfg, paukan) {
         .use(require('koa-response-time')())
         .use(require('koa-logger')())
         .use(require('koa-helmet').defaults())
+        .use(require('koa-helmet').xframe('SAMEORIGIN'))
         .use(require('koa-body')())
         .use(require('koa-static')(__dirname + '/../client'))
         .use(require('./routes')(cfg, paukan));
 
     var server = app.listen(cfg.listen);
     require('./rtsp')(server, cfg.rtsp);
-
+    paukan.log.info('Server started on port', cfg.listen);
     return app;
 };
